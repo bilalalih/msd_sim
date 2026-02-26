243857c1bf7cc9e4ca3f60ec83f5f938659471c4
Bilal Alih <bilalalih6@gmail.com>
2026-02-26 08:03:48 +0100

    Defined data structures

---
+++ include/msd/types.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace msd {
+
+  struct Params {
+    double m{1.0};
+    double c{0.2};
+    double k{10.0}
+  };
+
+  struct State {
+    double x{0.0};
+    double v{0.0};
+  };
+
+  struct Sample {
+    double t{0.0};
+    double x{0.0};
+    double v{0.0};
+    double a{0.0};
+    double u{0.0};
+    double E{0.0};
+  };
+
+} // namespace msd
