49271a9e6d59e7674ee9593e8fc4f6b141cba320
Bilal Alih <bilalalih6@gmail.com>
2026-02-26 08:05:34 +0100

    header file for physics logic

---
+++ include/msd/model.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "types.h"
+
+namespace msd {
+
+  double accel(const Params& p, const State& s, double u);
+  double energy(const Parmas& p, const State& s); // 0.5*m*v^2 + 0.5*k*v^2
+
+} // namespace msd
